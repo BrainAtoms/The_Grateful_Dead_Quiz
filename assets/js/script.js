896d6cca2782cc37a2e0850f3ffb8580a748c9ce
Brain Atoms <badams6453@gmail.com>
2023-02-15 18:24:08 -0500

    made initial text disappear when button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-const startingMinutes = 5; 
+var score = 0; 
+var question = "What's up, Doc?"
+
+const startingMinutes = 2; 
 let time = startingMinutes * 60; 
 
-const btnStart = document.getElementById('start-button')
+const btnStart = document.getElementById('start-button');
 const countdownEl = document.getElementById('countdown');
 
+
 btnStart.addEventListener('click', () => {
+    setInterval(updateCountdown, 1000);
+    btnStart.style.display = 'none'; 
 })
-
-
-setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
     const minutes = Math.floor(time / 60); 
@@ -16,9 +19,23 @@ function updateCountdown() {
 
     seconds = seconds < 10 ? '0' + seconds : seconds; 
 
-    countdownEl.innerHTML = `${minutes}:${seconds}`; 
+    time = time < 1 ? 1: time
+
+    countdownEl.innerHTML = "Timer:" + ` ${minutes}:${seconds}`; 
     time--;
 }
+
+function toggleText() {
+    var text = document.getElementById("main-text");
+    if (text.style.display === "block") {
+        text.innerHTML = question;
+    } else {
+        text.style.display = "block";
+    }
+    }
+
+
+
 
 
 
