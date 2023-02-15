ced835f6ebe823ce70040f9b5a2b302fa2d34a56
Brain Atoms <badams6453@gmail.com>
2023-02-15 14:50:05 -0500

    added CSS and countdown timer

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const startingMinutes = 5; 
+let time = startingMinutes * 60; 
+
+const btnStart = document.getElementById('start-button')
+const countdownEl = document.getElementById('countdown');
+
+btnStart.addEventListener('click', () => {
+})
+
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60); 
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds; 
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`; 
+    time--;
+}
+
+
+
+
+
