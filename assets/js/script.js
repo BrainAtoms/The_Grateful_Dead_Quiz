d2bb4be532eaeca458bcaee59a6b82398de023a8
Brain Atoms <badams6453@gmail.com>
2023-02-18 21:20:01 -0500

    deleted alt view scores page and fixed timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,89 +34,36 @@ const questions = [
   },
 ];
 
-const submittedScores = [];
-console.log(submittedScores);
+var buttonA = document.createElement("button");
+buttonA.setAttribute("id", "A");
+var buttonB = document.createElement("button");
+buttonB.setAttribute("id", "B");
+var buttonC = document.createElement("button");
+buttonC.setAttribute("id", "C");
+var buttonD = document.createElement("button");
+buttonD.setAttribute("id", "D");
 
-var score = 0;
-var currentIndex = 0;
-// const buttons = document.getElementsByClassName("button");
 const result = document.getElementById("result");
 const scoreEl = document.getElementById("score");
 const gameOver = document.getElementById("game-over");
+const submittedScores = [];
 const stats = document.getElementById("stats");
 
 var title = document.getElementById("title");
 var text = document.getElementById("main-text");
 var choices = document.getElementById("choices");
+var score = 0;
+var currentIndex = 0;
 
 const startingMinutes = 2;
 let time = startingMinutes * 60;
-
 const btnStart = document.getElementById("start-button");
 const countdownEl = document.getElementById("countdown");
 
 btnStart.addEventListener("click", () => {
-  setInterval(updateCountdown, 1000);
+  myInterval = setInterval(updateCountdown, 1000)
   btnStart.style.display = "none";
 });
-// buttonA = dcoument.getElementById('A')
-
-function goToScore() {
-  const scoresHTML = `<section><h1>Final Score: ${score}/${questions.length} </h1>
-<h1 class="scorepage">Add your initials to submit your score.</h1>
-<input class="scorepage" id="initials"/>
-<button class="scorepage" id="submit" onclick="onSubmitScore()";>Submit Score</button>
-<h2 class="scorepage" class="submitted">Submitted Scores:</h2>
-<div class="scorepage" id="high-scores"></div>
-<button class="scorepage" id="clear">Clear Scores</button>
-<button class="scorepage" onclick="restartQuiz()">Return to Coding Quiz</button></section>`;
-  var optionsEl = document.getElementById("options");
-  optionsEl.innerHTML = scoresHTML;
-  text.style.display = "none";
-  stats.style.display = "none";
-  title.style.display = "none";
-  time = 0;
-  const clearBtn = document.getElementById("clear");
-  const highScore = document.getElementById("high-scores")
-  clearBtn.addEventListener('click', () => {
-  highScore.innerHTML = ""; 
-  })
-}
-function restartQuiz() {
-  currentIndex = 0;
-  score = 0;
-  scoreEl.innerHTML = "Score: " + score;
-  time = startingMinutes * 60;
-  text.style.display = "block";
-  stats.style.display = "block";
-  result.innerHTML = "You got this!";
-  gameOver.textContent = '';
-  quesh();
-}
-
-const onAnswerClick = (event) => {
-  const clickedAnswer = event.target.innerHTML;
-  // console.log(clickedAnswer);
-  if (clickedAnswer === questions[currentIndex].correctAnswer) {
-    result.innerHTML = "Correct!";
-    score++;
-    scoreEl.innerHTML = "Score: " + score;
-  } else {
-    result.innerHTML = "Incorrect!";
-    score.innerHTML = "Score: " + score;
-    if (time >= 10) {
-      time = time - 10;
-    } else {
-      time = 0;
-    }
-  }
-  currentIndex++;
-  if (!questions[currentIndex]) {
-    goToScore();
-  } else {
-    quesh();
-  }
-};
 
 function updateCountdown() {
   const minutes = Math.floor(time / 60);
@@ -135,28 +82,6 @@ function updateCountdown() {
     time--;
   }
 }
-
-function onSubmitScore() {
-  const initialsInput = document.getElementById("initials");
-  const initials = initialsInput.value;
-  console.log(initials);
-  const newScore = { initials: initials, value: score };
-  const highScore = document.getElementById("high-scores");
-  submittedScores.push(newScore);
-  var submittedScoresHTML = submittedScores.map(function (score) {
-    return `<div>${score.initials}:</div><div>${score.value}</div>`;
-  });
-  highScore.innerHTML = submittedScoresHTML;
-}
-
-var buttonA = document.createElement("button");
-buttonA.setAttribute("id", "A");
-var buttonB = document.createElement("button");
-buttonB.setAttribute("id", "B");
-var buttonC = document.createElement("button");
-buttonC.setAttribute("id", "C");
-var buttonD = document.createElement("button");
-buttonD.setAttribute("id", "D");
 
 function quesh() {
   if (text.style.display === "block") {
@@ -185,5 +110,106 @@ function quesh() {
   buttonB.disabled = false;
   buttonC.disabled = false;
   buttonD.disabled = false;
- 
+}
+
+const onAnswerClick = (event) => {
+  const clickedAnswer = event.target.innerHTML;
+  // console.log(clickedAnswer);
+  if (clickedAnswer === questions[currentIndex].correctAnswer) {
+    result.innerHTML = "Correct!";
+    score++;
+    scoreEl.innerHTML = "Score: " + score;
+  } else {
+    result.innerHTML = "Incorrect!";
+    score.innerHTML = "Score: " + score;
+    if (time >= 10) {
+      time = time - 10;
+    } else {
+      time = 0;
+    }
+  }
+  currentIndex++;
+  if (!questions[currentIndex]) {
+    goToScore();
+  } else {
+    quesh();
+  }
+};
+
+function goToScore() {
+  const scoresHTML = `<section><h1>Final Score: ${score}/${questions.length} </h1>
+<h1 class="scorepage">Add your initials to submit your score.</h1>
+<input class="scorepage" id="initials"/>
+<button id="submit" class="scorepage" onclick="onSubmitScore()";>Submit Score</button>
+<div class="scorepage" id="high-scores"></div>
+<button class="scorepage" id="hide">Hide Scores</button>
+<button class="scorepage" onclick="restartQuiz()">Return to Coding Quiz</button></section>`;
+  var optionsEl = document.getElementById("options");
+  optionsEl.innerHTML = scoresHTML;
+  text.style.display = "none";
+  stats.style.display = "none";
+  title.style.display = "none";
+  clearInterval(myInterval);
+  const clearBtn = document.getElementById("hide");
+  const highScore = document.getElementById("high-scores")
+  clearBtn.addEventListener('click', () => {
+  highScore.innerHTML = "";
+  })
+}
+
+function onSubmitScore() {
+  const initialsInput = document.getElementById("initials");
+  const initials = initialsInput.value;
+  console.log(initials);
+  const newScore = { initials: initials, value: score };
+  const highScore = document.getElementById("high-scores");
+  submittedScores.push(newScore);
+  var submittedScoresHTML = submittedScores.map(function (score) {
+    return `<div>${score.initials}:</div><div>${score.value} out of 5</div>`;
+  });
+  highScore.innerHTML = submittedScoresHTML;
+  const submitBtn = document.getElementById("submit")
+  submitBtn.disabled = true; 
+}
+
+function restartQuiz() {
+  currentIndex = 0;
+  score = 0;
+  scoreEl.innerHTML = "Score: " + score;
+  time = startingMinutes * 60;
+  text.style.display = "block";
+  stats.style.display = "block";
+  result.innerHTML = "You got this!";
+  gameOver.textContent = '';
+  myInterval = setInterval(updateCountdown, 1000)
+  quesh();
+}
+
+function quesh() {
+  if (text.style.display === "block") {
+    text.innerHTML = questions[currentIndex].question + " ";
+  }
+  if (choices.style.display === "none") {
+    choices.style.display = "block";
+  }
+  if (choices.style.display === "block") {
+    if (currentIndex === 0) {
+      const optionsEl = document.getElementById("options");
+      optionsEl.replaceChildren(buttonA, buttonB, buttonC, buttonD);
+    }
+    buttonA.innerHTML = questions[currentIndex].choices[0];
+    buttonA.addEventListener("click", onAnswerClick);
+    buttonB.innerHTML = questions[currentIndex].choices[1];
+    buttonB.addEventListener("click", onAnswerClick);
+    buttonC.innerHTML = questions[currentIndex].choices[2];
+    buttonC.addEventListener("click", onAnswerClick);
+    buttonD.innerHTML = questions[currentIndex].choices[3];
+    buttonD.addEventListener("click", onAnswerClick);
+  } else {
+    question.style.display = "none";
+  }
+  buttonA.disabled = false;
+  buttonB.disabled = false;
+  buttonC.disabled = false;
+  buttonD.disabled = false;
 }
